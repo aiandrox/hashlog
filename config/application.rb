66554fd2e2b518996384a51ccfda8c95986d293e
Aiandrox <aiandrox7@gmail.com>
2020-03-24 19:55:14 +0900

    [Add] application.rbの設定

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,28 @@ module Hashlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # 言語・タイムゾーンを日本に設定
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    # generateで作成するファイルの制限
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        controller_specs: true,
+                        request_specs: false,
+                        fixtures: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
