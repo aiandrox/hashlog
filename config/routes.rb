61ee6fc855735210479132c212ece349e95e3132
Aiandrox <aiandrox7@gmail.com>
2020-03-28 21:18:27 +0900

    [Fix] Externalの設定不備を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   post "oauth/callback", to: "oauths#callback"
+  get 'oauth/callback', to: 'oauths#callback'
   get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
   resources :users, only: %i[create edit update show destroy]
 end
