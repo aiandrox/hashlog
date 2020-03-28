3e3891e40a0559f130be8b6f97a56217efb74ef0
Aiandrox <aiandrox7@gmail.com>
2020-03-28 14:34:52 +0900

    [Add] users_controllerを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show; end
+
+  def edit; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  resources :users, only: %i[create edit update show destroy]
 end
