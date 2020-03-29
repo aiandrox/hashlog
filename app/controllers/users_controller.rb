34da82151682540dbd2f84cdd8eac00fc425eec2
Aiandrox <aiandrox7@gmail.com>
2020-03-29 14:02:02 +0900

    [Add] ユーザー削除機能とログアウト機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def show; end
 
   def edit; end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    redirect_to root_path, notice: 'ユーザーを削除しました'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   post "oauth/callback", to: "oauths#callback"
   get 'oauth/callback', to: 'oauths#callback'
   get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
+  delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[create edit update show destroy]
 end
