6f86e7f3ed5af0a08b87ed078046003aa421193d
Aiandrox <aiandrox7@gmail.com>
2020-03-29 17:39:19 +0900

    [Fix] Rubocopの指摘を修正

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,7 +1,7 @@
 class Hashtag < ApplicationRecord
-    has_many :users, through: :user_hashtags
-    has_many :user_hashtags
-    accepts_nested_attributes_for :user_hashtags
+  has_many :users, through: :user_hashtags
+  has_many :user_hashtags
+  accepts_nested_attributes_for :user_hashtags
 
-    validates :name, presence: true, unique: true
+  validates :name, presence: true, unique: true
 end
