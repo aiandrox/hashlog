e19a8f818e8a3c696eaf1acc00f8c8d9130f4cdb
Aiandrox <aiandrox7@gmail.com>
2020-03-30 14:12:51 +0900

    [Fix] 誤字修正

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -3,5 +3,5 @@ class Hashtag < ApplicationRecord
   has_many :user_hashtags
   accepts_nested_attributes_for :user_hashtags
 
-  validates :name, presence: true, unique: true
+  validates :name, presence: true, uniqueness: true
 end
