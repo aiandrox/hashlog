65b66b6fa46e87820a7ed6bcdc4ca936b7cef9f3
Aiandrox <aiandrox7@gmail.com>
2020-03-30 18:02:04 +0900

    [Add] アソシエーションを追加

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,5 +1,6 @@
 class Hashtag < ApplicationRecord
   has_and_belongs_to_many :users
+  has_many :logs, through: :hashtags_users
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
 end
--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -12,5 +12,6 @@ RSpec.describe Hashtag, type: :model do
     end
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(100) }
   end
 end
