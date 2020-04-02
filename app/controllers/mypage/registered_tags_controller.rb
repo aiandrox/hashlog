c7c3d5fbef740763366d434b283265a49921f95b
Aiandrox <aiandrox7@gmail.com>
2020-04-02 16:33:24 +0900

    [Add] mypage registered_tagのCRUD

--- app/controllers/mypage/registered_tags_controller.rb
+++ app/controllers/mypage/registered_tags_controller.rb
@@ -1,5 +1,5 @@
 class Mypage::RegisteredTagsController < ApplicationController
-  before_action :set_tag, only: %i[show edit update]
+  before_action :set_tag, only: %i[show edit update destroy]
 
   def show
   end
@@ -8,11 +8,26 @@ class Mypage::RegisteredTagsController < ApplicationController
   end
 
   def update
+    if @tag.update(tag_params)
+      redirect_to mypage_registered_tag_path
+    else
+      flash.now[:alert] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @tag.destroy!
+    redirect_to mypage_path
   end
 
   private
 
   def set_tag
     @tag = current_user.registered_tags.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:registered_tag).permit(:privacy, :remind_day)
   end
 end
