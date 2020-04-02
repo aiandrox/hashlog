5761368ee1382f554b6c29eb6e41f777bac17dc3
Aiandrox <aiandrox7@gmail.com>
2020-04-02 12:55:13 +0900

    [Add] タグ登録機能を追加

--- app/controllers/mypage/tags_controller.rb
+++ app/controllers/mypage/tags_controller.rb
@@ -1,12 +1,12 @@
 class Mypage::TagsController < ApplicationController
   def new
     @tag = current_user.tags.new
+    @registered_tag = current_user.registered_tags.new
   end
 
   def create
-    if (@tag = Tag.find_or_create_by(tag_params))
-      current_user.registered_tags.create!(tag_id: @tag.id)
-      redirect_to mypage_path
+    if create_registered_tag
+      redirect_to mypage_path, notice: "#{@tag.name}を登録しました"
     else
       flash.now[:alert] = '登録できませんでした'
       render :new
@@ -14,6 +14,17 @@ class Mypage::TagsController < ApplicationController
   end
 
   private
+
+  def create_registered_tag
+    ActiveRecord::Base.transaction do
+      @tag = Tag.find_or_create_by!(tag_params)
+      @registered_tag = current_user.registered_tags.create!(tag_id: @tag.id)
+    rescue ActiveRecord::RecordInvalid
+      false
+    rescue
+      render status: 500
+    end
+  end
 
   def tag_params
     params.require(:tag).permit(:name)
--- app/models/registered_tag.rb
+++ app/models/registered_tag.rb
@@ -5,6 +5,7 @@ class RegisteredTag < ApplicationRecord
   validates :tweeted_day_count, presence: true
   validates :privacy, presence: true
   validates :remind_day, presence: true
+  validates :tag_id, uniqueness: { scope: :user_id, message: 'を既に登録しています' }
 
   enum privacy: { published: 0, closed: 1, limited: 2 }
 end
