fc6699c8feaf750dc49b245dc63b0f2380db5e62
Aiandrox <aiandrox7@gmail.com>
2020-04-03 15:21:11 +0900

    [Add] users, registered_tagsのリクエストスペックを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,10 @@ FactoryBot.define do
   factory :user do
     twitter_id { '1242379749650907137' }
     sequence(:name) { |n| "user_#{n}" }
+
+    after(:create) do |user|
+      create_list(:tag, 3, users: [user])
+    end
   end
 
   trait :guest do
---
+++ spec/requests/registered_tags_spec.rb
@@ -0,0 +1,13 @@
+describe 'RegisteredTags', type: :request do
+  let(:user) { create(:user) }
+  let(:tag) { user.tags.first }
+  let(:registered_tag) { create(:registered_tag, user: user, tag: tag) }
+  context "GET /users/:uuid/registered_tags/:id" do
+    it 'ユーザーのタグページを表示する' do
+      get user_registered_tag_path(user.uuid, registered_tag)
+      expect(response.status).to eq 200
+      expect(respense.body).to include user.name
+      expect(response.body).to include tag.name
+    end
+  end
+end
