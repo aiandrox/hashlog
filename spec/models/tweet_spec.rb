fc0e9cfcdb58b3b0615b6f982fe13cd0fa393440
Aiandrox <aiandrox7@gmail.com>
2020-04-04 00:32:57 +0900

    [Add] モデルスペックを追加

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { is_expected.to belong_to(:registered_tag) }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:content) }
+  end
 end
