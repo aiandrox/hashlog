6fce4fb599d041c05747fb57e7e9e3d712e6b415
Aiandrox <aiandrox7@gmail.com>
2020-04-09 13:52:31 +0900

    [Add] 雰囲気でmodelにjson取得のメソッドを書いてみた

--- app/models/registered_tag.rb
+++ app/models/registered_tag.rb
@@ -9,4 +9,35 @@ class RegisteredTag < ApplicationRecord
   validates :tag_id, uniqueness: { scope: :user_id, message: 'を既に登録しています' }
 
   enum privacy: { published: 0, closed: 1, limited: 2 }
+
+  # curl -X POST "https://api.twitter.com/1.1/tweets/search/30day/dev.json"\
+  # -d '{"query":"#自炊 from:aiandrox","maxResults":"100"}'\
+  # -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAOYRDQEAAAAAqOj4OL9iYGT0oYU5gk16giPvUe4%3DZ83FP6MTMJrYI8McD0SEbwkMMys8IKgyud9M4ExUADj8KgOJGU"
+  def tweets
+    
+  end
+
+  def connection
+    api_key = Rails.application.credentials.twitter[:key]
+    api_secret = Rails.application.credentials.twitter[:secret_key]
+    token = Rails.application.credentials.twitter[:access_token]
+    token_secret = Rails.application.credentials.twitter[:access_token_secret]
+    url = 'https://api.twitter.com/1.1/tweets/search/30day/dev.json'
+    Faraday.new(url) do |faraday|
+      faraday.request :url_encoded
+      faraday.request :oauth, { consumer_key: api_key,
+                                consumer_secret: api_secret,
+                                token: token,
+                                token_secret: token_secret }
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def response_result
+    res = connection.post do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = { query: "#自炊 from:aiandrox", maxResults: '100' }.to_json
+    end
+    body = JSON.parse res.body
+  end
 end
