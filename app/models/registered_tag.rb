ecb4ececbaded9300175d7c48a0aea42a01b9f17
Aiandrox <aiandrox7@gmail.com>
2020-04-09 15:39:22 +0900

    [Add] twiiter gemを用いてプレミアムツイート取得機能を追加

--- app/models/registered_tag.rb
+++ app/models/registered_tag.rb
@@ -17,7 +17,7 @@ class RegisteredTag < ApplicationRecord
       config.access_token        = Rails.application.credentials.twitter[:access_token]
       config.access_token_secret = Rails.application.credentials.twitter[:access_token_secret]
     end
-    client.search("##{tag.name} from:aiandrox", result_type: "recent").take(100).collect do |date|
+    client.search("##{tag.name} from:#{user.screen_name}", result_type: "recent").take(100).collect do |date|
       tweet = Tweet.new(registered_tag_id: id) # TODO　アソシエーションがだめ。tweet = tweets.buildがダメだった
       tweet.content = date.text
       tweet.created_at = date.created_at
@@ -25,41 +25,17 @@ class RegisteredTag < ApplicationRecord
     end
   end
 
-
-
-
-
-
-
-  
-  # curl -X POST "https://api.twitter.com/1.1/tweets/search/30day/dev.json"\
-  # -d '{"query":"#自炊 from:aiandrox","maxResults":"100"}'\
-  # -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAOYRDQEAAAAAqOj4OL9iYGT0oYU5gk16giPvUe4%3DZ83FP6MTMJrYI8McD0SEbwkMMys8IKgyud9M4ExUADj8KgOJGU"
-  def tweets
-    
-  end
-
-  def connection
-    api_key = Rails.application.credentials.twitter[:key]
-    api_secret = Rails.application.credentials.twitter[:secret_key]
-    token = Rails.application.credentials.twitter[:access_token]
-    token_secret = Rails.application.credentials.twitter[:access_token_secret]
-    url = 'https://api.twitter.com/1.1/tweets/search/30day/dev.json'
-    Faraday.new(url) do |faraday|
-      faraday.request :url_encoded
-      faraday.request :oauth, { consumer_key: api_key,
-                                consumer_secret: api_secret,
-                                token: token,
-                                token_secret: token_secret }
-      faraday.adapter Faraday.default_adapter
+  def premium_tweet
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.credentials.twitter[:key]
+      config.consumer_secret     = Rails.application.credentials.twitter[:secret_key]
+      config.dev_environment     = 'dev'
     end
-  end
-
-  def response_result
-    res = connection.post do |req|
-      req.headers['Content-Type'] = 'application/json'
-      req.body = { query: "#自炊 from:aiandrox", maxResults: '100' }.to_json
+    client.premium_search("##{tag.name} from:#{user.screen_name}", { maxResults: 100 }, { product: '30day' }).take(100).collect do |date|
+      tweet = Tweet.new(registered_tag_id: id) # TODO　アソシエーションがだめ。tweet = tweets.buildがダメだった
+      tweet.content = date.text
+      tweet.created_at = date.created_at
+      tweet.save!
     end
-    body = JSON.parse res.body
   end
 end
