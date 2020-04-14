83edd4e94c216cbbf2fcc560167b2c3ef2686227
Aiandrox <aiandrox7@gmail.com>
2020-04-14 21:06:56 +0900

    [Test] use_transactional_fixtures = false https://net-vacation.com/blog/archives/7608

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 Rails.logger = Logger.new(STDOUT) # Rails.loggerを出す
-# ActiveRecord::Base.logger = Logger.new(STDOUT) # SQLログ出す
+ActiveRecord::Base.logger = Logger.new(STDOUT) # SQLログ出す
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -44,7 +44,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
