4213b737152db9e04d93ea35b6200ec9d4c53bf5
Aiandrox <aiandrox7@gmail.com>
2020-04-20 13:43:44 +0900

    [Add] ESLintを追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { environment } = require("@rails/webpacker");
+const { VueLoaderPlugin } = require("vue-loader");
+const vue = require("./loaders/vue");
+const eslint = require("./loaders/eslint");
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment
+environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
+environment.loaders.prepend("vue", vue);
+environment.loaders.append("eslint", eslint);
+module.exports = environment;
