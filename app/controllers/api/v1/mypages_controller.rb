64bdfc092e7c4d85aa5f8bc5b9a817b0a1358489
Aiandrox <aiandrox7@gmail.com>
2020-04-20 13:43:30 +0900

    [Fix] Rubocop指摘修正

--- app/controllers/api/v1/mypages_controller.rb
+++ app/controllers/api/v1/mypages_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::MypagesController < ApplicationController
   def show
     user = current_user
-    registered_tags = user.registered_tags.joins(:tag).select('registered_tags.id, name, tweeted_day_count, privacy, remind_day, first_tweeted_at, last_tweeted_at')
+    registered_tag_params = 'registered_tags.id, name, tweeted_day_count,
+                             privacy, remind_day, first_tweeted_at, last_tweeted_at'
+    registered_tags = user.registered_tags
+                          .joins(:tag)
+                          .select(registered_tag_params)
     result_values = {
       user: user,
       registered_tags: registered_tags
