4054fe589c6285dd0b3b68ab46c6e29b0fd8e9aa
Aiandrox <aiandrox7@gmail.com>
2020-04-21 21:45:15 +0900

    [Fix] エンドポイントを一つに統一

--- app/controllers/api/v1/mypage/registered_tags_controller.rb
+++ app/controllers/api/v1/mypage/registered_tags_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::Mypage::RegisteredTagsController < Api::V1::Mypage::BaseController
   def show
+    registered_tag_params = 'registered_tags.id, name, tweeted_day_count,
+                             privacy, remind_day, first_tweeted_at, last_tweeted_at'
+    registered_tags = current_user.registered_tags
+                                  .joins(:tag)
+                                  .select(registered_tag_params)
     registered_tag = current_user.registered_tags.find(params[:id])
     tweets = registered_tag.tweets
     result_values = {
       user: current_user,
-      registered_tags: registered_tag,
+      registered_tags: registered_tags,
+      registered_tag: registered_tag,
       tweets: tweets
     }
     render json: result_values
@@ -33,10 +39,6 @@ class Api::V1::Mypage::RegisteredTagsController < Api::V1::Mypage::BaseControlle
   end
 
   private
-
-  def user_params
-    params.require(:user).permit(:id)
-  end
 
   def tag_params
     params.require(:tag).permit(:privacy, :remind_day, :name)
--- app/controllers/api/v1/mypages_controller.rb
+++ app/controllers/api/v1/mypages_controller.rb
@@ -1,13 +1,12 @@
 class Api::V1::MypagesController < Api::V1::BaseController
   def show
-    user = current_user
     registered_tag_params = 'registered_tags.id, name, tweeted_day_count,
                              privacy, remind_day, first_tweeted_at, last_tweeted_at'
-    registered_tags = user.registered_tags
-                          .joins(:tag)
-                          .select(registered_tag_params)
+    registered_tags = current_user.registered_tags
+                                  .joins(:tag)
+                                  .select(registered_tag_params)
     result_values = {
-      user: user,
+      user: current_user,
       registered_tags: registered_tags
     }
     render json: result_values
