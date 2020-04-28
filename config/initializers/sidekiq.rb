45d2aa14fd8490196e9829b72cc4500e557d9661
Aiandrox <aiandrox7@gmail.com>
2020-04-29 00:51:13 +0900

    [Fix] RubocopとCirleCI（Rubyのバージョン）の修正

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,10 +12,10 @@ end
 Sidekiq.configure_client do |config|
   config.redis = case Rails.env
                  when 'production'
-                   config.redis = { url: 'redis://prd.redis-example.com:6379', namespace: 'sidekiq' }
+                   { url: 'redis://prd.redis-example.com:6379', namespace: 'sidekiq' }
                  when 'staging'
-                   config.redis = { url: 'redis://stg.redis-example.com:6379', namespace: 'sidekiq' }
+                   { url: 'redis://stg.redis-example.com:6379', namespace: 'sidekiq' }
                  else
-                   config.redis = { url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
+                    url: 'redis://127.0.0.1:6379', namespace: 'sidekiq' }
                  end
 end
