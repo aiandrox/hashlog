195cd8adbbe49cf1cc98ec6b1f3e325697013dc1
Aiandrox <aiandrox7@gmail.com>
2020-04-28 13:19:41 +0900

    [Remove] Delayed Job削除

--- Gemfile
+++ Gemfile
@@ -19,7 +19,6 @@ gem 'rails-i18n'
 gem 'slim-rails'
 
 # 機能
-gem 'delayed_job_active_record'
 gem 'sorcery'
 gem 'whenever', require: false
 # api
--- app/models/registered_tag.rb
+++ app/models/registered_tag.rb
@@ -21,14 +21,13 @@ class RegisteredTag < ApplicationRecord
   def add_tweets
     last_tweet = tweets.desc.first
     return create_tweets unless last_tweet
-    return if last_tweet.tweeted_at > Date.yesterday
-
+    # return if last_tweet.tweeted_at > Date.yesterday
     since_id = last_tweet.tweet_id.to_i
     client = TwitterAPI::Client.new(user, tag.name, since_id)
     client.tweets_data('everyday').each do |oembed, tweeted_at, tweet_id|
       tweets.create(oembed: oembed, tweeted_at: tweeted_at, tweet_id: tweet_id)
     end
-    return if client.tweets_data('everyday').empty?
+    # return if client.tweets_data('everyday').empty?
 
     fetch_data
     Rails.logger.info("#{user.screen_name}の#{tag.name}にツイートを追加")
