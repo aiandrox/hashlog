76bc779c1e455f906d1180e42bcba07be2e430a4
Aiandrox <aiandrox7@gmail.com>
2020-05-01 00:38:09 +0900

    [Improve] Tweetモデルにlatest, oldestメソッドを追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,15 @@ class Tweet < ApplicationRecord
 
   validates :oembed, presence: true
   validates :tweet_id, presence: true
+  validates :tweet_id, uniqueness: { scope: :registered_tag_id }
+
+  scope :desc, -> { order(tweeted_at: :desc) }
+
+  def self.latest
+    desc.first
+  end
+
+  def self.oldest
+    desc.last
+  end
 end
