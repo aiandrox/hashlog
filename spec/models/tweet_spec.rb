a4fabfc251e9a8040a2d5022a7b5d1336cbc2a28
Aiandrox <aiandrox7@gmail.com>
2020-05-01 13:57:02 +0900

    [Add] Tweetモデルのスコープ、メソッドのスペックを追加

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,10 +1,42 @@
 RSpec.describe Tweet, type: :model do
-  context 'associations' do
+  describe 'associations' do
     it { is_expected.to belong_to(:registered_tag) }
   end
 
-  context 'validations' do
+  describe 'validations' do
     it { is_expected.to validate_presence_of(:oembed) }
     it { is_expected.to validate_presence_of(:tweet_id) }
+  end
+
+  describe 'scopes' do
+    let!(:latest_tweet) { create(:tweet, tweeted_at: DateTime.now) }
+    let!(:oldest_tweet) { create(:tweet, tweeted_at: DateTime.yesterday) }
+    describe 'desc' do
+      it 'tweeted_atを基準に昇順に並ぶこと' do
+        expect(Tweet.desc.first).to eq latest_tweet
+        expect(Tweet.desc.last).to eq oldest_tweet
+      end
+    end
+    describe 'tweeted_day_count' do
+      before { create(:tweet, tweeted_at: DateTime.now.ago(7.day))}
+      it 'tweetが存在する日数を返す' do
+        expect(Tweet.tweeted_day_count).to eq 3
+      end
+    end
+  end
+
+  describe 'methods' do
+    let!(:latest_tweet) { create(:tweet, tweeted_at: DateTime.now) }
+    let!(:oldest_tweet) { create(:tweet, tweeted_at: DateTime.yesterday) }
+    describe 'self.latest' do
+      it 'tweeted_atを基準に最も新しいツイートを返す' do
+        expect(Tweet.latest).to eq latest_tweet
+      end
+    end
+    describe 'self.oldest' do
+      it 'tweeted_atを基準に最も古いツイートを返す' do
+        expect(Tweet.oldest).to eq oldest_tweet
+      end
+    end
   end
 end
