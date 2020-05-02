6e617ac25c1309548fc929b07d4ce599bf43fb9e
Aiandrox <aiandrox7@gmail.com>
2020-05-02 18:16:26 +0900

    [Remove?] 削除予定のリクエストスペックをpendingにする

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,6 +66,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  config.include ActiveJob::TestHelper
+  config.include ActiveSupport::Testing::TimeHelpers
   config.include UserSessionsHelper
 end
 
