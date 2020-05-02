23c023bff9256225f6b08912d46e11a735076ca6
Aiandrox <aiandrox7@gmail.com>
2020-05-02 19:58:38 +0900

    [Remove] AddTweetsJob#performのスペックを削除

--- spec/jobs/add_tweets_job_spec.rb
+++ spec/jobs/add_tweets_job_spec.rb
@@ -9,23 +9,4 @@ RSpec.describe AddTweetsJob, type: :job do
       expect { AddTweetsJob.perform_later }.to have_enqueued_job.at(:no_wait)
     end
   end
-
-  xdescribe 'AddTweetsJob#perform' do
-    let(:job) { AddTweetsJob.new }
-    let(:registered_tag) { RegisteredTag.all.sample }
-    before { create(:user, :with_tags) }
-    it 'ログが出力される' do
-      expect(Rails.logger).to receive(:info).once
-      VCR.use_cassette('twitter_api/everyday_search_all_tweets', record: :new_episodes) do
-        job.perform
-      end
-    end
-    it 'RegisteredTag#add_tweetsを実行する' do
-      allow_message_expectations_on_nil
-      expect(registered_tag).to receive(:add_tweets)
-      VCR.use_cassette('twitter_api/everyday_search_all_tweets') do
-        job.perform
-      end
-    end
-  end
 end
