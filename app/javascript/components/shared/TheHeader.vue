09ea303a1f92da9b75acafa0676ce6e94ce01922
Aiandrox <aiandrox7@gmail.com>
2020-05-04 19:15:42 +0900

    [Fix] マイページから遷移した場合URLが相対パスになるのを修正

--- app/javascript/components/shared/TheHeader.vue
+++ app/javascript/components/shared/TheHeader.vue
@@ -1,14 +1,9 @@
 <template>
-  <v-app-bar
-    app
-    flat
-    dark
-    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-  >
+  <v-app-bar app flat dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
     <v-toolbar-title>Hashlog</v-toolbar-title>
     <v-spacer />
     <v-toolbar-items>
-      <v-btn text href="api/v1/oauth/twitter">ログイン</v-btn>
+      <v-btn text href="/api/v1/oauth/twitter">ログイン</v-btn>
       <v-btn text :to="{ name: 'mypage' }">マイページ</v-btn>
       <v-btn text @click="logout">ログアウト</v-btn>
     </v-toolbar-items>
