17f4fab1a571fb568be9965a0f12a123121a69da
Aiandrox <aiandrox7@gmail.com>
2020-05-04 12:07:24 +0900

    [Add] BaseControllerのスペックに追加

--- spec/requests/api/v1/base_spec.rb
+++ spec/requests/api/v1/base_spec.rb
@@ -1,21 +1,31 @@
 RSpec.describe 'Base', type: :request do
   let(:user) { create(:user) }
 
-  context 'rescue_limited_twitter_requests' do
-    it 'TwitterAPIのリクエストが上限に達した場合 エラーメッセージを返す' do
-      client = TwitterAPI::Client.new(user, 'ハッシュタグ')
-      allow(client).to receive(:tweets_data).and_raise(Twitter::Error::TooManyRequests)
-      client.tweets_data('standard')
-      expect(response.status).to eq 429
-      expect(response.body).to include 'リクエストが集中しています。15分後に再度試してください'
+  describe 'rescue_limited_twitter_requests' do
+    context 'TwitterAPIのリクエストが上限に達した場合' do
+      let(:client) { TwitterAPI::Client.new(user, 'ハッシュタグ') }
+      before do
+        allow(client).to receive(:tweets_data).and_raise(Twitter::Error::TooManyRequests)
+        client.tweets_data('standard')
+      end
+      it '429 TooManyRequestsを返す' do
+        expect(response.status).to eq 429
+      end
+      it 'エラーメッセージのJSONを返す' do
+        expect(response.body).to include 'リクエストが集中しています。15分後に再度試してください'
+      end
     end
   end
 
-  context 'not_authenticated' do
-    it 'ログインしていない場合 エラーメッセージを返す' do
-      get '/api/v1/mypage'
-      expect(response.status).to eq 401
-      expect(response.body).to include 'ログインしてください'
+  describe 'not_authenticated' do
+    context 'ログインしていない場合' do
+      before { get '/api/v1/mypage' }
+      it '401 Unauthorizedを返す' do
+        expect(response.status).to eq 401
+      end
+      it 'エラーメッセージのJSONを返す' do
+        expect(response.body).to include 'ログインしてください'
+      end
     end
   end
 end
