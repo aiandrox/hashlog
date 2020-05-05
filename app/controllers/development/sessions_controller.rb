6114dad6d9bfa83d7fb7b5fcdacc4e4775d775f2
Aiandrox <aiandrox7@gmail.com>
2020-05-05 13:32:42 +0900

    [Fix] login_asを修正

--- app/controllers/development/sessions_controller.rb
+++ app/controllers/development/sessions_controller.rb
@@ -2,6 +2,6 @@ class Development::SessionsController < ActionController::Base
   def login_as
     user = User.find(params[:user_id])
     auto_login(user)
-    redirect_to mypage_path, notice: "#{Rails.env}環境でログインしました"
+    redirect_to mypage_dashboard_path, notice: "#{Rails.env}環境でログインしました"
   end
 end
