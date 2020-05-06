3bf53ad888ad5f773719da5559da62238b1794f6
Aiandrox <aiandrox7@gmail.com>
2020-05-06 18:16:23 +0900

    [Add] User#registered_tagのスペックを追加

--- app/controllers/api/v1/registered_tags_controller.rb
+++ app/controllers/api/v1/registered_tags_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::RegisteredTagsController < Api::V1::BaseController
   def create
     tag = Tag.find_or_initialize_by(name: tag_params[:name])
     if current_user.register_tag(tag)
-      registered_tag = current_user.registered_tag(tag: tag)
+      registered_tag = current_user.registered_tag(tag)
       result_values = {
         flash: {
           type: 'success',
@@ -24,7 +24,7 @@ class Api::V1::RegisteredTagsController < Api::V1::BaseController
   end
 
   def destroy
-    registered_tag = RegisteredTag.find(params[:id])
+    registered_tag = current_user.registered_tags.find(params[:id])
     registered_tag.destroy!
     result_values = {
       flash: {
