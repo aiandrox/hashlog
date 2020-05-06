d34294bd79ba256af6ade62f530907176fb54ea0
Aiandrox <aiandrox7@gmail.com>
2020-05-06 15:51:02 +0900

    [FIx] twitter_api_specの修正

--- spec/services/twitter_api_spec.rb
+++ spec/services/twitter_api_spec.rb
@@ -1,13 +1,12 @@
 RSpec.describe TwitterAPI::Client do
   let(:user) { create(:user, :real_value) }
   describe '#tweets_data' do
-    context '該当のツイートがない場合' do
+    context '該当のツイートがない場合',
+      vcr: { cassette_name: 'twitter_api/standard_search/該当のツイートがない場合' } do
       let(:tag) { create(:tag, name: 'absent_tag') }
       let(:client) { TwitterAPI::Client.new(user, tag.name) }
       it '空の配列を返す' do
-        VCR.use_cassette('twitter_api/standard_search_with_absent_tag') do
-          expect(client.tweets_data).to eq([])
-        end
+        expect(client.tweets_data).to eq([])
       end
     end
 
@@ -32,7 +31,7 @@ RSpec.describe TwitterAPI::Client do
       end
       context '"standard"を引数に渡すとき' do
         let(:type) { 'standard' }
-        let(:client) { twitter_client(user, tag.name) }
+        let(:client) { TwitterAPI::Client.new(user, tag.name) }
         it '#standard_searchを実行する' do
           set_twitter_mock(user, tag.name)
           expect(client).to receive(:standard_search).once
@@ -42,7 +41,7 @@ RSpec.describe TwitterAPI::Client do
       end
       context '"premium"を引数に渡すとき' do
         let(:type) { 'premium' }
-        let(:client) { twitter_client(user, tag.name) }
+        let(:client) { TwitterAPI::Client.new(user, tag.name) }
         it '#premiun_searchを実行する' do
           set_twitter_mock(user, tag.name)
           expect(client).to receive(:premium_search).once
@@ -53,9 +52,9 @@ RSpec.describe TwitterAPI::Client do
       context '"everyday"を引数に渡すとき' do
         let(:type) { 'everyday' }
         let(:since_id) { '1255854602626330624' }
-        let(:client) { twitter_client(user, tag.name, since_id) }
+        let(:client) { TwitterAPI::Client.new(user, tag.name, since_id) }
         it '#everyday_searchを実行する' do
-          set_twitter_mock(user, tag.name)
+          set_twitter_mock(user, tag.name, since_id)
           expect(client).to receive(:everyday_search).once
           client.tweets_data(type)
         end
