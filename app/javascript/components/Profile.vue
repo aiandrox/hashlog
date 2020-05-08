cf852f7c2531021b3c3373603ff6f25d028d09d4
Aiandrox <aiandrox7@gmail.com>
2020-05-08 14:32:56 +0900

    [Improve] ユーザー情報をテンプレートに切り出し

--- app/javascript/components/Profile.vue
+++ app/javascript/components/Profile.vue
@@ -3,30 +3,12 @@
     <v-card class="mx-auto" outlined>
       <v-container class="d-flex flex-row">
         <div>
-          <v-list-item>
-            <v-list-item-avatar color="grey" size="60">
-              <v-img
-                src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-              />
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title class="title">
-                {{ user.name }}
-                <v-btn icon color="blue" :href="twitterUrl">
-                  <v-icon>mdi-twitter</v-icon>
-                </v-btn>
-              </v-list-item-title>
-              <v-list-item-subtitle>@{{ user.screenName }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-card-text>
-            <div class="body-1">{{ user.description }}</div>
-          </v-card-text>
+          <profile-user :user="user"></profile-user>
           <v-card-actions>
-            <v-btn class="ma-2" outlined color="success">
+            <v-btn class="ma-2" outlined>
               <v-icon left>mdi-pencil</v-icon>編集
             </v-btn>
-            <v-btn class="ma-2" outlined color="success" @click="showDialog">
+            <v-btn class="ma-2" outlined @click="showDialog">
               <v-icon left>mdi-pound</v-icon>ハッシュタグを登録する
             </v-btn>
           </v-card-actions>
@@ -44,11 +26,13 @@
 <script>
 import ProfileStatus from "./ProfileStatus"
 import RegisterTagDialog from "./TheRegisterTagDialog"
+import ProfileUser from "./ProfileUser"
 export default {
   data() {
     return { loading: false }
   },
   components: {
+    ProfileUser,
     ProfileStatus,
     RegisterTagDialog
   },
