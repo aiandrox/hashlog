b0e08e211793c1eefb110df4c51e6802de904541
Aiandrox <aiandrox7@gmail.com>
2020-05-14 12:50:45 +0900

    [Add] タグ登録時のバリデーション追加

--- app/javascript/components/TheRegisterTagDialog.vue
+++ app/javascript/components/TheRegisterTagDialog.vue
@@ -1,73 +1,95 @@
 <template>
   <v-row justify-center>
     <v-dialog v-model="dialog" max-width="600px">
-      <v-card>
-        <v-system-bar
-          class="pa-5 subtitle-1"
-        >ハッシュタグを登録する</v-system-bar>
-        <v-card-text class="mt-5">
-          <v-container>
-            <v-text-field
-              v-model="tagName"
-              label="ハッシュタグ"
-              hint="#を含めず入力してください"
-              persistent-hint
-              required
+      <ValidationObserver ref="observer" v-slot="{ invalid }">
+        <v-card>
+          <v-system-bar class="pa-5 subtitle-1"
+            >ハッシュタグを登録する</v-system-bar
+          >
+
+          <v-card-text class="mt-5">
+            <v-container>
+              <v-form>
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  name="ハッシュタグ"
+                  rules="required"
+                >
+                  <v-text-field
+                    v-model="tagName"
+                    :error-messages="errors"
+                    label="ハッシュタグ"
+                    hint="#を含めず入力してください"
+                    persistent-hint
+                    required
+                  >
+                    <v-icon slot="prepend">mdi-pound</v-icon>
+                  </v-text-field>
+                </ValidationProvider>
+              </v-form>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn text @click="pushCancel">キャンセル</v-btn>
+            <v-btn text color="primary" @click="sendTagName" :disabled="invalid"
+              >登録する</v-btn
             >
-              <v-icon slot="prepend">mdi-pound</v-icon>
-            </v-text-field>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn text @click="dialog = false">キャンセル</v-btn>
-          <v-btn text color="primary" @click="sendTagName">登録する</v-btn>
-        </v-card-actions>
-      </v-card>
+          </v-card-actions>
+        </v-card>
+      </ValidationObserver>
       <!-- ローディング -->
-      <loading v-show="loading" />
+      <loading-view v-show="isLoading" />
     </v-dialog>
   </v-row>
 </template>
 
 <script>
 import axios from "axios"
-import loading from "./shared/TheLoading"
+import loadingView from "./shared/TheLoading"
+
 export default {
   components: {
-    loading
+    loadingView
   },
   data() {
     return {
       dialog: false,
       tagName: "",
-      loading: false
+      isLoading: false
     }
   },
   methods: {
     open() {
       this.dialog = true
     },
-    sendTagName() {
-      this.loading = true
-      axios
-        .post("/api/v1/registered_tags", {
-          tag: {
-            name: this.tagName
-          }
-        })
-        .then(response => {
-          const tagId = response.data.registeredTag.id
+    pushCancel() {
+      this.dialog = false
+      this.tagName = ""
+      this.$nextTick(() => {
+        this.$refs.observer.reset()
+      })
+    },
+    async sendTagName() {
+      const isValid = await this.$refs.observer.validate()
+      // disableにしているから大丈夫のはずだけど、一応バリデーションを通らないとリクエストを送れないようにしている
+      if (isValid) {
+        try {
+          this.isLoading = true
+          const registeredTagRes = await axios.post("/api/v1/registered_tags", {
+            tag: {
+              name: this.tagName
+            }
+          })
+          const tagId = registeredTagRes.data.registeredTag.id
           this.dialog = false
           this.tagName = ""
           this.$router.push({ name: "mypageTag", params: { id: tagId } })
-        })
-        .catch(response => {
-          console.log(response)
-        })
-        .finally(() => {
-          this.loading = false
-        })
+        } catch (error) {
+          console.log(error)
+        }
+        this.isLoading = false
+      }
     }
   }
 }
