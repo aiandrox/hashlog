5fa48c5327be8b7c2d181ef066aebae43b628d6f
Aiandrox <aiandrox7@gmail.com>
2020-05-30 16:12:32 +0900

    [Add] ページによってタブの表示、リンク先を変更する

--- app/javascript/components/TagsTab.vue
+++ app/javascript/components/TagsTab.vue
@@ -2,16 +2,16 @@
   <v-container row>
     <div col="12" lg="10">
       <v-tabs background-color="#e9f1f5" show-arrows>
-        <v-tab :to="{ name: 'mypage' }">マイページ</v-tab>
+        <v-tab :to="homePositionRoute">{{ homePositionName }}</v-tab>
         <v-tab
           v-for="registeredTag in registeredTags"
           :key="registeredTag.id"
-          :to="{ name: 'myTag', params: { tagId: registeredTag.id } }"
+          :to="registeredTagRoute(registeredTag)"
         >#{{ registeredTag.tag.name }}</v-tab>
       </v-tabs>
     </div>
     <v-spacer />
-    <div col="2">
+    <div col="2" v-if="isMypage">
       <v-btn class="ma-2" color="primary" depressed @click="pushRegister">
         <v-icon left>mdi-pound</v-icon>ハッシュタグを登録する
       </v-btn>
@@ -22,7 +22,9 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex"
 import registerTagDialog from "../components/TheRegisterTagDialog"
+
 export default {
   components: {
     registerTagDialog
@@ -33,7 +35,33 @@ export default {
       default: () => []
     }
   },
+  computed: {
+    ...mapGetters({ pageType: "page/type" }),
+    isMypage() {
+      return this.pageType === "mypage"
+    },
+    homePositionName() {
+      if (this.pageType === "mypage") {
+        return "マイページ"
+      }
+      return "ユーザーページ"
+    },
+    homePositionRoute() {
+      if (this.pageType === "mypage") {
+        return { name: "mypage" }
+      }
+      const userUuid = this.$route.params.userUuid
+      return { name: "user", params: { userUuid } }
+    }
+  },
   methods: {
+    registeredTagRoute(registeredTag) {
+      if (this.pageType === "mypage") {
+        return { name: "myTag", params: { tagId: registeredTag.id } }
+      }
+      const userUuid = this.$route.params.userUuid
+      return { name: "userTag", params: { userUuid, tagId: registeredTag.id } }
+    },
     pushRegister() {
       this.$refs.registerDialog.open()
     }
