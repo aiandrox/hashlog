9bfea62485a0ab70333d355579a6fbb35b17256d
Aiandrox <aiandrox7@gmail.com>
2020-05-31 21:55:39 +0900

    [Design] ツイートの表示崩れを修正

--- app/javascript/pages/MyTag.vue
+++ app/javascript/pages/MyTag.vue
@@ -4,8 +4,7 @@
     <the-tab :registered-tags="registeredTags" />
     <v-container class="main-content d-flex flex-row-reverse pt-0" row>
       <!-- ハッシュタグの情報 -->
-
-      <v-col class="hidden-sm-and-down" cols="12" md>
+      <v-col class="hidden-sm-and-down" cols="12" md="4">
         <v-card flat>
           <tag-status
             ref="tagStatus"
@@ -33,7 +32,7 @@
     <!-- 削除ダイアログ -->
     <delete-dialog ref="deleteDialog" @push-delete="deleteTag">
       保存されていたツイートのデータが
-      <br>全て消えてしまいます。
+      <br />全て消えてしまいます。
     </delete-dialog>
   </div>
 </template>
