f4e080c5cbb5bb421f6757a25b65114f1b41999b
Aiandrox <aiandrox7@gmail.com>
2020-06-03 21:47:37 +0900

    [Add] ログアウトのアクションにrequired_loginを追加

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -6,11 +6,6 @@ class Api::V1::BaseController < ApplicationController
 
   rescue_from Twitter::Error::TooManyRequests, with: :rescue_limited_twitter_requests
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
-  # rescue_from ActionController::RoutingError, with: :rescue_not_found
-
-  # def routing_error
-  #   raise ActionController::RoutingError, params[:path]
-  # end
 
   protected
 
