212d86f85fe39372a0d28a6a36d6c80653b7403e
Aiandrox <aiandrox7@gmail.com>
2020-06-04 00:05:14 +0900

    [Add] gem bankenの追加、セットアップ

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,11 +1,13 @@
 require 'pagy/extras/headers'
 
 class Api::V1::BaseController < ApplicationController
+  include Banken
   include Pagy::Backend
   after_action :set_pagy_header, if: -> { @pagy }
 
-  rescue_from Twitter::Error::TooManyRequests, with: :rescue_limited_twitter_requests
+  rescue_from Banken::NotAuthorizedError, with: :not_authorized
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
+  rescue_from Twitter::Error::TooManyRequests, with: :rescue_limited_twitter_requests
 
   protected
 
@@ -14,13 +16,22 @@ class Api::V1::BaseController < ApplicationController
     response.headers.merge!({ 'Request-Url' => request.path_info })
   end
 
-  def rescue_limited_twitter_requests
+  def not_authenticated
     error_json = {
-      'status' => '429',
-      'title' => 'Twitter APIが制限されています。',
-      'detail' => '15分後に再度試してください。'
+      'status' => '401',
+      'title' => '認証されていません。',
+      'detail' => 'ログインしてください。'
     }
-    render json: { 'error': error_json }, status: 429 # TooManyRequests
+    render json: { 'error': error_json }, status: 401 # Unauthorized
+  end
+
+  def not_authorized
+    error_json = {
+      'status' => '403',
+      'title' => '閲覧できません。',
+      'detail' => 'リソースへのアクセスが許可されていません。'
+    }
+    render json: { 'error': error_json }, status: 403 # Forbidden
   end
 
   def rescue_not_found
@@ -32,12 +43,12 @@ class Api::V1::BaseController < ApplicationController
     render json: { 'error': error_json }, status: 404 # NotFound
   end
 
-  def not_authenticated
+  def rescue_limited_twitter_requests
     error_json = {
-      'status' => '401',
-      'title' => '認証されていません。',
-      'detail' => 'ログインしてください。'
+      'status' => '429',
+      'title' => 'Twitter APIが制限されています。',
+      'detail' => '15分後に再度試してください。'
     }
-    render json: { 'error': error_json }, status: 401 # Unauthorized
+    render json: { 'error': error_json }, status: 429 # TooManyRequests
   end
 end
