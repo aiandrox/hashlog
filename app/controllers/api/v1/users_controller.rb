52250595739d72ce4852eab15621821d609b704a
Aiandrox <aiandrox7@gmail.com>
2020-06-04 14:27:55 +0900

    [Add] Userリソースの制限を追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,13 @@ class Api::V1::UsersController < Api::V1::BaseController
   before_action :require_login, only: %i[update destroy]
 
   def index
-    @pagy, users = pagy(User.all)
+    @pagy, users = pagy(User.published)
     render json: users
   end
 
   def show
     user = User.find_by(uuid: params[:uuid])
+    authorize!(user)
     render json: user
   end
 
