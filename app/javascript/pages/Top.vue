94cfb274bd33443930b40e6ab545ab5c981af84a
Aiandrox <aiandrox7@gmail.com>
2020-06-05 13:01:43 +0900

    [Add] 人気ハッシュタグをトップページで取得

--- app/javascript/pages/Top.vue
+++ app/javascript/pages/Top.vue
@@ -2,35 +2,26 @@
   <div>
     <v-btn text href="/api/v1/oauth/twitter">ログイン</v-btn>
     <v-btn text :to="{ name: 'mypage' }">マイページ</v-btn>
-    <v-btn text :to="{ name: 'user', params: { userUuid: 'HTagphtz9-sM' } }"
-      >ユーザーページ</v-btn
-    >
+    <v-btn text :to="{ name: 'user', params: { userUuid: 'HTagphtz9-sM' } }">ユーザーページ</v-btn>
     <v-btn text>ログアウト</v-btn>
+    人気ハッシュタグ{{ tags }}
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    name: "",
-    email: "",
-    select: null,
-    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    checkbox: null
+    tags: []
   }),
   mounted() {
     document.title = "Hashlog"
+    this.fetchTagsData()
   },
   methods: {
-    submit() {
-      this.$refs.observer.validate()
-    },
-    clear() {
-      this.name = ""
-      this.email = ""
-      this.select = null
-      this.checkbox = null
-      this.$refs.observer.reset()
+    async fetchTagsData() {
+      const tagsRes = await this.$axios.get("/api/v1/tags?count=3")
+      const { tags } = tagsRes.data
+      this.tags = tags
     }
   }
 }
