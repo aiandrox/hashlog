c87a2631b6441ab7c59b16fac2573ccdd3317b07
Aiandrox <aiandrox7@gmail.com>
2020-06-05 16:48:57 +0900

    [Improve] 公開非公開設定の認可ではなく、リソースの絞り込みで対応

--- app/controllers/api/v1/users/registered_tags_controller.rb
+++ app/controllers/api/v1/users/registered_tags_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::Users::RegisteredTagsController < Api::V1::BaseController
   def index
-    user = User.find_by!(uuid: params[:user_uuid])
+    user = User.published.find_by!(uuid: params[:user_uuid])
     registered_tags = user.registered_tags.published.asc.includes(:tag)
-    authorize!(user)
-    @pagy, registered_tags = pagy(registered_tags)
     render json: registered_tags
   end
 end
