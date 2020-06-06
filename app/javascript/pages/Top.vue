8cec74861a26a9c49711500084f828e59391adbe
Aiandrox <aiandrox7@gmail.com>
2020-06-06 13:23:22 +0900

    [Add] トップページ完成

--- app/javascript/pages/Top.vue
+++ app/javascript/pages/Top.vue
@@ -1,58 +1,81 @@
 <template>
   <div class="top">
-    <div class="main-header">
-      <!-- メインヘッダー -->
+    <!-- メインヘッダー -->
+    <div class="main-header white-zone">
       <v-container>
-        <v-row>
-          <v-col class="header-left text-right align-self-center" :md="5">
-            <v-img class="logo" alt="Hashlog" max-width="300" src="/img/logo.png" />
-            <p>いい感じのキャッチコピー</p>
-            <v-btn color="primary" x-large depressed @click="pushLogin">今すぐ始める</v-btn>
-
-            <p>人気のハッシュタグ</p>
-            <p>
-              <span v-for="tag in tags" :key="tag.id">#{{ tag.name + " " }}</span>
+        <v-row class="mt-md-10">
+          <v-col class="text-right align-self-center" :md="5">
+            <v-img class="logo" alt="Hashlog" src="/img/logo.png" />
+            <p class="title">
+              ハッシュタグで
+              <br class="d-block d-sm-none" />あなたの学びをキチンとする
             </p>
-            <v-btn color="#42a9db" dark depressed @click="guestLogin">ゲストとして試してみる</v-btn>
+            <v-btn color="primary" x-large depressed @click="pushLogin">今すぐ始める</v-btn>
+            <div class="d-none d-sm-block">
+              <p class="mt-10 mb-1 title">人気のハッシュタグ</p>
+              <span v-for="tag in popularTags" :key="tag.id">#{{ tag.name + " " }}</span>
+            </div>
+            <v-btn
+              class="register-btn"
+              color="#42a9db"
+              dark
+              depressed
+              @click="guestLogin"
+            >ゲストとして試してみる</v-btn>
           </v-col>
           <v-col>
-            <v-img max-width="600" src="/img/main-image.png" />
+            <v-img class="register-btn" max-width="600" src="/img/main-image.png" />
           </v-col>
         </v-row>
       </v-container>
     </div>
-    <v-container class="main-content">
-      <v-row>
+    <!-- 機能紹介 -->
+    <v-container class="main-content features">
+      <v-row
+        v-for="(description, index) in descriptions"
+        :key="index"
+        :class="'flex-row'+description.line"
+      >
         <v-col class="align-self-center" :md="7">
-          <h2 class="mb-5">あなたはハッシュタグを付けて呟くだけ</h2>
-          <p>ハッシュタグを登録した後は、あなたは今までと同じように、Twitterでハッシュタグを付けてツイートするだけです。</p>
-          <p>Hashlogがあなたのツイートを自動で収集します。</p>
+          <h2 class="mb-5">{{description.title}}</h2>
+          <div v-html="description.text" />
         </v-col>
         <v-col :md="5">
-          <v-img alt="あなたはハッシュタグを付けて呟くだけ" src="/img/hashtag.png" />
-        </v-col>
-      </v-row>
-      <v-row class="flex-row-reverse">
-        <v-col class="align-self-center" :md="7">
-          <h2 class="mb-5">ツイートをした日をカレンダーに記録</h2>
-          <p>あなたがツイートした日をわかりやすく記録します。</p>
-          <p>学習記録がひと目でわかります。</p>
-        </v-col>
-        <v-col :md="5">
-          <v-img alt="ツイートをした日をカレンダーに記録" src="/img/calendar.png" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="align-self-center" :md="7">
-          <h2 class="mb-5">リマインダーで「うっかり忘れ」を防止</h2>
-          <p>もし、あなたが一定日数ツイートをしなかった場合、Hashlogからリプライが届きます。</p>
-          <small>事前に1〜30日でリマインダーを設定している必要があります。</small>
-        </v-col>
-        <v-col :md="5">
-          <v-img alt="リマインダーで「うっかり忘れ」を防止" src="/img/time.png" />
+          <v-img :alt="description.title" :src="description.imgSrc" />
         </v-col>
       </v-row>
     </v-container>
+    <!-- 最近登録されたハッシュタグ -->
+    <div class="white-zone pb-10">
+      <v-container>
+        <h2 class="text-center mb-5 mt-8">最近登録されたハッシュタグ</h2>
+        <v-row>
+          <v-col cols="12" md="4" v-for="tag in recentTags" :key="tag.id">
+            <v-card
+              outlined
+              :to="{name: 'userTag', params: {tagId: tag.id, userUuid: tag.user.uuid}}"
+            >
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1 mt-3">#{{tag.tag.name}}</v-list-item-title>
+
+                  <v-list-item-subtitle class="mb-3 text-right">by {{tag.user.name}}</v-list-item-subtitle>
+
+                  <v-list-item-text>
+                    <p>
+                      初めてのツイート:
+                      <br />
+                      {{date(tag.firstTweetedAt)}}
+                    </p>
+                    <p>ツイート総日数: {{tag.tweetedDayCount}}日（{{tag.tweetRate}}%）</p>
+                  </v-list-item-text>
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
     <!-- 利用規約ダイアログ -->
     <the-terms-dialog ref="termsDialog" />
   </div>
@@ -67,7 +90,35 @@ export default {
   },
   data() {
     return {
-      tags: []
+      popularTags: [],
+      recentTags: []
+    }
+  },
+  computed: {
+    descriptions() {
+      return [
+        {
+          title: "あなたはハッシュタグを付けて呟くだけ",
+          imgSrc: "/img/hashtag.png",
+          text:
+            "<p>ハッシュタグを登録した後は、あなたは今までと同じように、Twitterでハッシュタグを付けてツイートするだけです。</p><p>Hashlogがあなたのツイートを自動で収集します。</p>",
+          line: ""
+        },
+        {
+          title: "ツイートをした日をカレンダーに記録",
+          imgSrc: "/img/calendar.png",
+          text:
+            "<p>ツイートをした日はカレンダーに印が付きます。</p><p>あなたの学習記録がひと目でわかります。</p>",
+          line: "-reverse"
+        },
+        {
+          title: "リマインダーで「うっかり忘れ」を防止",
+          imgSrc: "/img/time.png",
+          text:
+            "<p>もし、あなたが一定日数ツイートをしなかったときは、Hashlogからリプライが届きます。</p><small>※ 事前にリマインダーを設定しておく必要があります。</small>",
+          line: ""
+        }
+      ]
     }
   },
   mounted() {
@@ -79,9 +130,14 @@ export default {
       this.$refs.termsDialog.open()
     },
     async fetchTagsData() {
-      const tagsRes = await this.$axios.get("/api/v1/tags?count=3")
-      const { tags } = tagsRes.data
-      this.tags = tags
+      const popularTagsRes = await this.$axios.get("/api/v1/tags?count=3")
+      const popularTags = popularTagsRes.data.tags
+      this.popularTags = popularTags
+      const recentTagsRes = await this.$axios.get(
+        "/api/v1/registered_tags?count=3"
+      )
+      const recentTags = recentTagsRes.data.registeredTags
+      this.recentTags = recentTags
     },
     async guestLogin() {
       await this.$store.dispatch("user/guestLogin")
@@ -90,7 +146,12 @@ export default {
         message: "ゲストユーザーとしてログインしました"
       })
       this.$router.push({ name: "mypage" })
-      this.$toTop()
+    },
+    date(date) {
+      if (date === null) {
+        return "まだツイートはありません"
+      }
+      return this.$dayjs(date)
     }
   }
 }
@@ -100,11 +161,22 @@ export default {
 .top {
   color: #3b394d;
 }
-.main-header {
+.logo {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.register-btn {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.white-zone {
   background-color: #fff;
+}
+.main-header {
+  min-height: 100vh;
   border-bottom: 20px solid #006596;
 }
-.main-content p {
+.features p {
   line-height: 1.8rem;
 }
 </style>
