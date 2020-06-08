1332917c0623d5f7913015035deff0a93aba58d4
Aiandrox <aiandrox7@gmail.com>
2020-06-08 14:03:30 +0900

    [Remove] 開発用ツイート削除アクションを削除

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -10,17 +10,4 @@ class Api::V1::TweetsController < Api::V1::BaseController
     @pagy, tweets = pagy(tweets, items: 10) if params[:page]
     render json: tweets
   end
-
-  # 開発時 & 管理者用
-  def destroy
-    tweet = Tweet.find(params[:id])
-    tweet.destroy!
-    result_values = {
-      flash: {
-        type: 'success',
-        message: 'ツイートを削除しました'
-      }
-    }
-    render json: result_values
-  end
 end
