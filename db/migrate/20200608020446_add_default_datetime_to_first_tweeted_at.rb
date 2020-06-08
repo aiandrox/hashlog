df17b81041ef1ceedd4f68599dfc1d84aefa363a
Aiandrox <aiandrox7@gmail.com>
2020-06-08 14:18:24 +0900

    [Remove] RegisteredTag#first_tweeted_atで仮想的にnilを返すのが無理だったので元に戻す

--- db/migrate/20200608020446_add_default_datetime_to_first_tweeted_at.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultDatetimeToFirstTweetedAt < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default :registered_tags, :first_tweeted_at, from: nil, to: Time.parse('2999-12-31')
-  end
-end
