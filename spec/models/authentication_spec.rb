8015396f4ef8fbcf2c2920bcad4b0d40444b2b24
Aiandrox <aiandrox7@gmail.com>
2020-06-13 14:10:33 +0900

    [Fix] 新規作成時にデータがコピーされるようにする

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -1,4 +1,4 @@
-RSpec.fdescribe Authentication, type: :model do
+RSpec.describe Authentication, type: :model do
   describe 'associations' do
     it 'belongs_to :user' do
       is_expected.to belong_to(:user)
