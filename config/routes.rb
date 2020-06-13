2ac32d6a58e26b8b38a0eb4eb094c57a800f65cb
Aiandrox <aiandrox7@gmail.com>
2020-06-13 10:19:37 +0900

    [Add] 継続率ランキングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'mypage/tags/:id', to: 'static_pages#top'
   get 'users/:uuid', to: 'static_pages#top'
   get 'users/:uuid/tags/:id', to: 'static_pages#top'
+  get 'ranking/persistence', to: 'static_pages#top'
 
   namespace :api do
     namespace :v1 do
