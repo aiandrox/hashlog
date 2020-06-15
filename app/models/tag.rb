0a78a2dac9be41120a771c15d6b285fac2296372
Aiandrox <aiandrox7@gmail.com>
2020-06-15 11:42:12 +0900

    [Fix] ハッシュタグに使えない文字列を登録できないようにバリデーションを追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,6 +4,7 @@ class Tag < ApplicationRecord
   has_many :registered_tags, dependent: :restrict_with_error
   has_many :users, through: :registered_tags
 
+  validates :name, format: { with: /\A[Ａ-Ｚａ-ｚA-Za-z一-龠々0-9０-９ぁ-ヶｦ-ﾟー゛゜_]+\Z/, message: 'が不適な形式です' }
   validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
 
   # TODO: 警告を直す
@@ -13,5 +14,7 @@ class Tag < ApplicationRecord
 
   def remove_first_hashtag_mark
     name&.gsub!(/^#+/, '')
+    # 数字_゛゜だけの場合はバリデーションで弾くために、'@'に置き換える。ゴリ押しっぽいけど…
+    name&.gsub!(/\A[\d０-９゛゜_]+\Z/, '@')
   end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -16,6 +16,45 @@ RSpec.describe Tag, type: :model do
     it 'name: length <100' do
       is_expected.to validate_length_of(:name).is_at_most(100)
     end
+    describe 'name: hashtag_format' do
+      context '正常系' do
+        it 'ひらがな' do
+          is_expected.to allow_value('ひらがな', 'ぁ').for(:name)
+        end
+        it 'カタカナ' do
+          is_expected.to allow_value('カタカナ', 'ヴ').for(:name)
+        end
+        it '漢字' do
+          is_expected.to allow_value('漢字', '々').for(:name)
+        end
+        it 'Alphabet' do
+          is_expected.to allow_value('Alphabet', 'A', 'aaa').for(:name)
+        end
+        it '123半角数字と文字' do
+          is_expected.to allow_value('123半角数字と文字', '1あ2あ3').for(:name)
+        end
+        it '１２３全角数字と文字' do
+          is_expected.to allow_value('１２３全角数字と文字', '1２３あ').for(:name)
+        end
+        it '_゛゜と文字' do
+          is_expected.to allow_value('_文字_', '゛゛あ゛', '゜゜あ゜').for(:name)
+        end
+      end
+      context '異常系' do
+        it '記号混じり' do
+          is_expected.not_to allow_value('あ#あ', 'あ@あ', 'あ-あ', 'あ。あ', 'あ、あ', 'あ,あ', 'あ.あ').for(:name)
+        end
+        it '数字のみ' do
+          is_expected.not_to allow_value('111', '１１１').for(:name)
+        end
+        it '_゛゜のみ' do
+          is_expected.not_to allow_value('_', '___', '゛', '゜').for(:name)
+        end
+        it 'ああ あ（スペース混じり）' do
+          is_expected.not_to allow_value('あ　あ', 'あ あ', ' あ', '　あ').for(:name)
+        end
+      end
+    end
   end
 
   describe 'scope' do
@@ -37,6 +76,7 @@ RSpec.describe Tag, type: :model do
 
   describe 'methods' do
     describe '#remove_first_hashtag_mark' do
+      # tagのバリデーションのための部分は上の'name: hashtag_format'の項目でテストする
       context '先頭に#が入っているとき' do
         let(:tag) { build(:tag, name: '#ハッシュタグ') }
         it '#を消した文字列になる' do
