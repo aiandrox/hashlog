4f960bde4d2c102ac33a4f0421f26d706cb12f42
Aiandrox <aiandrox7@gmail.com>
2020-06-16 16:14:27 +0900

    [Fix] エラーを修正 ・ランキングのロジック ・access_tokenなどの取得の仕方

--- app/controllers/api/v1/registered_tags/persistences_controller.rb
+++ app/controllers/api/v1/registered_tags/persistences_controller.rb
@@ -1,8 +1,9 @@
+require 'pagy/extras/array'
+
 class Api::V1::RegisteredTags::PersistencesController < Api::V1::BaseController
   def index
-    registered_tags = RegisteredTag.includes(:user, :tag).opened.have_tweets
-    @pagy, registered_tags = pagy(registered_tags)
-    registered_tags = registered_tags.persistence_sort
+    registered_tags = RegisteredTag.includes(:user, :tag).opened.have_tweets.persistence_sort
+    @pagy, registered_tags = pagy_array(registered_tags)
     render json: registered_tags
   end
 end
