1d3b906bac4db0c55d1add1c57e007b27fd2ca36
Aiandrox <aiandrox7@gmail.com>
2020-08-30 10:09:06 +0900

    [Fix] 文面を修正

--- app/jobs/remind_reply_job.rb
+++ app/jobs/remind_reply_job.rb
@@ -1,7 +1,7 @@
 class RemindReplyJob < ApplicationJob
   queue_as :default
 
-  # TODO: 重複してリプライを送ることになる。制限は300リクエストなので保留
+  # TODO: 重複してメンションを送ることになる。制限は300リクエストなので保留
   # rescue_from Twitter::Error::TooManyRequests do
   #   logger.error("RemindReplyJob: Twitter::Error::TooManyRequests\n3時間後に再試行します")
   #   retry_job wait: 3.hours, queue: :low_priority
