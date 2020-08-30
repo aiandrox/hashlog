b5991e2ad43b4d18e8cdfcd77b1d73b0475e4911
Aiandrox <aiandrox7@gmail.com>
2020-08-30 11:14:23 +0900

    [Fix] サービスクラス用にスペックを修正

--- app/services/cron_twitter.rb
+++ app/services/cron_twitter.rb
@@ -1,25 +1,12 @@
 class CronTwitter
   include LoggerHelper
 
-  def initialize(type)
-    case type
-    when :add_tweets
-      self.job = TwitterAPIJob::AddTweets.new
-    when :remind_reply
-      self.job = TwitterAPIJob::RemindReply.new
-    end
+  def call(twitter_api_job)
+    logger.info("\n#{Time.now}")
+    twitter_api_job.call
+    message = twitter_api_job.notify_logs.join("\n")
+    slack_notify("#{message}")
+  rescue StandardError => e
+    slack_notify(e)
   end
-
-  def call
-    logger.info("\n#{Time.now} : #{type}")
-    job.call
-    message = job.notify_logs.join("\n")
-    slack_notify("#{type}\n#{message}")
-  rescue Twitter::Error::TooManyRequests
-    slack_notify("#{type}: Twitter::Error::TooManyRequests")
-  end
-
-  private
-
-  attr_accessor :type, :job
 end
--- lib/tasks/twitter_cron.rake
+++ lib/tasks/twitter_cron.rake
@@ -1,11 +1,11 @@
 namespace :twitter_cron do
   desc 'TwitterAPIによる自動検索'
   task add_tweets: :environment do
-    CronTwitter.new(:add_tweets).call
+    CronTwitter.new.call(TwitterAPIJob::AddTweets.new)
   end
 
   desc 'TwitterAPIによる自動ツイート'
   task remind_reply: :environment do
-    CronTwitter.new(:remind_reply).call
+    CronTwitter.new.call(TwitterAPIJob::RemindReply.new)
   end
 end
