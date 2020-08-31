ec6f316cf11ffbf828d0e038726bc020e6da77e7
Aiandrox <aiandrox7@gmail.com>
2020-08-31 16:43:36 +0900

    [Fix] schedule.rbのタスク名を変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
 
 every 1.day, at: '6:00 am' do
-  rake 'twitter_cron:search_tweets_everyday'
+  rake 'twitter_cron:add_tweets'
 end
 
 every 1.day, at: '7:00 am' do
