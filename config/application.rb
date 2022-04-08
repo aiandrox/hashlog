93f5c9701cd46fd2f3dc58f41f32450e0b7a0e43
Aiandrox <aiandrox7@gmail.com>
2022-04-09 02:14:08 +0900

    bundle exec rails app:update

--- config/application.rb
+++ config/application.rb
@@ -22,16 +22,19 @@ module Hashlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
     # 言語・タイムゾーンを日本に設定
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
     # generateで作成するファイルの制限
     config.generators do |g|
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[password access_token access_token_secret]
+Rails.application.config.filter_parameters += %i[
+  password access_token access_token_secret passw secret token _key crypt salt certificate otp ssn
+]
